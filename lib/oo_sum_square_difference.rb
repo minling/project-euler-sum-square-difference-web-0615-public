37374a2d97900bed8d86cf4d435e34be6f58b963
minling <mzhao725@gmail.com>
2015-07-13 10:14:27 -0400

    done

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,28 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+
+  attr_accessor :num
+
+  def initialize(num)
+    @num = num
+  end
+
+  def sum_squares
+    numbers = (1..num).to_a
+    sum = numbers.collect do |number|
+    number * number
+    end
+    sum.reduce(:+)
+  end
+
+  def square_sum
+    numbers = (1..num).to_a
+    sum = numbers.reduce(:+)
+    sum * sum
+  end
+
+  def difference
+    square_sum - sum_squares
+  end
+
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,23 @@
 # Implement your procedural solution here!
+
+
+# The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... + 10^2 = 385
+# The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 55^2 = 3025
+# Hence the difference between the square of the sum and the sum of the squares is 3025 − 385 = 2640. Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+  def sum_squares(num)
+    numbers = (1..num).to_a
+    sum = numbers.collect do |number|
+    number * number
+    end
+    sum.reduce(:+)
+  end
+
+  def square_sum(num)
+    numbers = (1..num).to_a
+    sum = numbers.reduce(:+)
+    sum * sum
+  end
+
+  def sum_square_difference(num)
+    square_sum(num) - sum_squares(num)
+  end
